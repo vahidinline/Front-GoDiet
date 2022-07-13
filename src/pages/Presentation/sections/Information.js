4cbe06f28a9e3b82a75a956b0f4d1562fda9c87f
vahid Afshari <vahidinline@gmail.com>
2022-07-13 15:06:01 +0300

    first commit

---
+++ src/pages/Presentation/sections/Information.js
@@ -0,0 +1,104 @@
+/*
+=========================================================
+* Material Kit 2 React - v2.0.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-kit-react
+* Copyright 2021 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// @mui material components
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+
+// Material Kit 2 React components
+import MKBox from "components/MKBox";
+
+// Material Kit 2 React examples
+import RotatingCard from "examples/Cards/RotatingCard";
+import RotatingCardFront from "examples/Cards/RotatingCard/RotatingCardFront";
+import RotatingCardBack from "examples/Cards/RotatingCard/RotatingCardBack";
+import DefaultInfoCard from "examples/Cards/InfoCards/DefaultInfoCard";
+
+// Images
+import bgFront from "assets/images/rotating-card-bg-front.jpeg";
+import bgBack from "assets/images/rotating-card-bg-back.jpeg";
+
+function Information() {
+  return (
+    <MKBox component="section" py={6} my={6}>
+      <Container>
+        <Grid container item xs={11} spacing={3} alignItems="center" sx={{ mx: "auto" }}>
+          <Grid item xs={12} lg={4} sx={{ mx: "auto" }}>
+            <RotatingCard>
+              <RotatingCardFront
+                image={bgFront}
+                icon="touch_app"
+                title={
+                  <>
+                    Feel the
+                    <br />
+                    Freedom{" "}
+                  </>
+                }
+                description="You don't need to follow aggressive diet plan to lose weight. Just Change your habbit"
+              />
+              <RotatingCardBack
+                image={bgBack}
+                title="Discover More"
+                description="Azar will help you to find best eating habbit"
+                action={{
+                  type: "internal",
+                  route: "/sections/page-sections/page-headers",
+                  label: "start the journey",
+                }}
+              />
+            </RotatingCard>
+          </Grid>
+          <Grid item xs={12} lg={7} sx={{ ml: "auto" }}>
+            <Grid container spacing={3}>
+              <Grid item xs={12} md={6}>
+                <DefaultInfoCard
+                  icon="content_copy"
+                  title="Taking the Control"
+                  description="Built new habbit in order to lose weight and gain muscle at the same time"
+                />
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <DefaultInfoCard
+                  icon="flip_to_front"
+                  title="Budget Friendly"
+                  description="No need to spend much"
+                />
+              </Grid>
+            </Grid>
+            <Grid container spacing={3} sx={{ mt: { xs: 0, md: 6 } }}>
+              <Grid item xs={12} md={6}>
+                <DefaultInfoCard
+                  icon="Impressive results "
+                  title="Save Time & Money"
+                  description="Creating your meal plan from scratch"
+                />
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <DefaultInfoCard
+                  icon="Be your Expert"
+                  title="Fully knowledge"
+                  description="Regardless of the screen size, the website content will naturally fit the given resolution."
+                />
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Container>
+    </MKBox>
+  );
+}
+
+export default Information;
